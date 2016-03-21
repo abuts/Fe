af06543dbc77fa9efe1971118885eb9298b6c186
Alex Buts <Alex.Buts@stfc.ac.uk>
2016-03-21 15:12:43 +0000

    simple changes to use new symmetrization routine

--- Feb2016/sqw/symmetrEi200_110.m
+++ Feb2016/sqw/symmetrEi200_110.m
@@ -2,8 +2,15 @@ data_source= fullfile(pwd,'Data','Fe_ei200.sqw');
 proj.u = [1,0,0];
 proj.v = [0,1,0];
 pos = [1,1,0];
-qstep=qe_range(-1,0.05,1);
+
+mff = MagneticIons('Fe0');
+
+fixer = @(ws)(mff.fix_magnetic_ff(ws));
+
 erange = [0,2,200];
-outfile = fullfile(pwd,'Data','Fe_ei200Sym110.sqw');
-zonelist = {[1,1,0],[1,-1,0],[-1,1,0],[0,1,1],[0,1,-1],[0,-1,1],[1,0,1],[1,0,-1],[-1,0,1]};
-combine_equivalent_zones(data_source,proj,pos,qstep,erange,outfile,zonelist);
+outfile = fullfile(pwd,'Data','Fe_ei200Sym110_test1.sqw');
+zonelist = {[1,1,0],[1,-1,0]}; %{[1,1,0],[1,-1,0],[-1,1,0],[0,1,1],[0,1,-1],[0,-1,1],[1,0,1],[1,0,-1],[-1,0,1]};
+tansf_list = combine_equivalent_zones(data_source,proj,pos,...
+    {[-0.1,0.01,0.1],[-0.1,0.01,0.1],[-0.1,0.01,0.1]},erange,outfile,zonelist,...
+    'symmetry_type','shift','correct_fun',fixer);    
+%    {[-0.1,0.01,0.1],[-0.1,0.01,0.1],[-0.1,0.01,0.1]},erange,outfile,zonelist);    
