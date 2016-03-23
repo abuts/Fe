d649cebac2874641e0ca4582fb6cb42b5881622d
Alex Buts <Alex.Buts@stfc.ac.uk>
2016-03-23 11:00:09 +0000

    Added all symmetrisation scripts
    Former-commit-id: 8e4c9e2be68efe933d1f87632e3ffd898161499b

---
+++ Feb2016/sqw/symmetrEi200_110_test.m
@@ -0,0 +1,19 @@
+data_source= fullfile(pwd,'Data','Fe_ei200.sqw');
+proj.u = [1,0,0];
+proj.v = [0,1,0];
+pos = [1,1,0];
+
+mff = MagneticIons('Fe0');
+fixer = @(ws)(mff.fix_magnetic_ff(ws));
+
+erange = [0,2,200];
+outfile = fullfile(pwd,'Data','Fe_ei200shift110test.sqw');
+zonelist = {[1,1,0],[1,-1,0]}; %,[-1,1,0],[0,1,1],[0,1,-1],[0,-1,1],...
+
+%zonelist = {[1,1,0],[1,-1,0],[-1,1,0],[0,1,1],[0,1,-1],[0,-1,1],...
+%    [1,0,1],[1,0,-1],[-1,0,1]}; %,...
+    %[2,0,0],[-2,0,0],[0,2,0],[0,-2,0],[0,0,2],[0,0,2]};
+tansf_list = combine_equivalent_zones(data_source,proj,pos,...
+    {[-1,0.04,1],[-1,0.04,1],[-1,0.04,1]},erange,outfile,zonelist,...
+    'symmetry_type','shift','correct_fun',fixer);    
+%    {[-0.1,0.01,0.1],[-0.1,0.01,0.1],[-0.1,0.01,0.1]},erange,outfile,zonelist);    
