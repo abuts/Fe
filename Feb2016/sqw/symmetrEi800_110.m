358849ba010b39e970767414478ec0d2d26d8e0c
Alex Buts <Alex.Buts@stfc.ac.uk>
2016-06-09 12:30:56 +0100

    minor changes build on linux cluster
    Former-commit-id: 1bdfa64cca62a2cc79bfa9ec89e9bef36fa68611

---
+++ Feb2016/sqw/symmetrEi800_110.m
@@ -0,0 +1,10 @@
+data_source= fullfile(pwd,'Data','Fe_ei787.sqw');
+proj.u = [1,0,0];
+proj.v = [0,1,0];
+pos = [1,1,0];
+qstep=qe_range(-1,0.04,1);
+erange = [0,10,785];
+outfile = fullfile(pwd,'Data','Fe_ei800Sym110.sqw');
+set(hor_config,'accumulating_process_num',9);
+zonelist = {[1,1,0],[1,-1,0],[-1,1,0],[0,1,1],[0,1,-1],[0,-1,1],[1,0,1],[1,0,-1],[-1,0,1]};
+combine_equivalent_zones(data_source,proj,pos,qstep,erange,outfile,zonelist);
