fcc9db0d22101a8704179dfe74ad0294560f3fbb
Alex Buts <Alex.Buts@stfc.ac.uk>
2017-06-13 12:40:48 +0100

    wide range calculation script defining primary cuts around all Ei200 directions

--- June2017/Ei200_uvw100_at1m10TPmod2Peaks.m
+++ June2017/Ei200_uvw100_at1m10TPmod2Peaks.m
@@ -3,45 +3,24 @@ function Ei200_uvw100_at1m10TPmod2Peaks(varargin)
 data_source= fullfile(pwd,'sqw','Data','Fe_ei200.sqw');
 
 avrg_par=[0,0,949.35];
+bragg = [1,-1,0];
+dE = 5;
+dK = 0.05;
 
-if nargin == 0
-    repPoints1 = [-0.2625,-0.0875,0,0.2875,0.3375;
-        95,17,0,67,121];   
-    repPoints2= [-0.3375,-0.2625,0,0.0875,0.3125;
-        123,73,0,13,97];
-    repPoints3 = [-0.3375,-0.1125,0,0.0875,0.3125;
-        109,13,0,13,109];
-    rp1=parWithCorrections(repPoints1);
-    rp1.fix_x_coordinate=false;
-else
-    repPoints1 = [   -0.2625 ,  -0.1125,         0    ,0.1375 ,   0.3580;
-        76.2500,   17.1092,    2.0244,   17.6590,  121.0000];
-    
-    repPoints2= [ -0.2875 ,  -0.0875 ,        0  ,  0.1375 ,   0.3375;
-        81.7839 ,  13.0408  ,  6.1338  , 23.7725,  111.5941];
-    repPoints3 = [ -0.3125 ,  -0.1125  ,       0,    0.1375,    0.2500;
-        105.3054, 19.4796, 4.1003, 17.4702, 54.7273];
-    rp1=parWithCorrections(repPoints1);
-    rp1.cut_at_e_points = true;
-    rp1.energies = varargin{1};
-    %rp1.p = avrg_par;
-    rp1.fix_x_coordinate = false;    
-    
-end
+repPoints1 = [-0.2625,-0.0875,0,0.2875,0.3375;
+    95,15,0,65,120];
+repPoints2= [-0.3375,-0.2625,0,0.0875,0.3125;
+    125,70,0,15,100];
+repPoints3 = [-0.3375,-0.1125,0,0.0875,0.3125;
+    110,15,0,15,110];
 % Cuts
 % #1
-rp1.cut_direction=[1,0,0];
-rp1.dE = 5;
-rp1.dk = 0.05;
+rp1 = parWithCorrections(repPoints1,bragg,[1,0,0],dE,dK);
 % #2
-rp2 = parWithCorrections(rp1);
-rp2 = rp2.set_refpar(repPoints2);
-rp2.cut_direction=[0,1,0];
+rp2 = parWithCorrections(repPoints2,bragg,[0,1,0],dE,dK);
 % #3
-rp3 = parWithCorrections(rp1);
-rp3 = rp3.set_refpar(repPoints3);
-rp3.cut_direction=[0,0,1];
+rp3 = parWithCorrections(repPoints3,bragg,[0,0,1],dE,dK);
 
 repP={rp1,rp2,rp3};
 
-do_fits(data_source,[1,-1,0],'<1,0,0>',repP)
+do_fits(data_source,bragg,'<1,0,0>',repP)
--- June2017/Ei200_uvw110_at110TPmod2Peaks.m
+++ June2017/Ei200_uvw110_at110TPmod2Peaks.m
@@ -4,82 +4,41 @@ data_source= fullfile(pwd,'sqw','Data','Fe_ei200.sqw');
 
 avrg_par=[0,0,957.9503];
 bragg = [1,1,0];
+dE = 5;
+dK = 0.05;
 
 
 
-if nargin == 0
     repPoints1= [-0.2625,-0.0625,0,0.1375,0.3875;
-        87,13,0,15,123];
+        90,15,0,15,125];
     repPoints2 = [-0.3125,-0.1375,0,0.1375,0.3375;
-        109,17,0,15,109];
+        110,15,0,15,110];
     repPoints3 = [-0.2625,-0.0875,0,0.1125,0.3875;
-        101,11,0,11,119];
+        100,15,0,15,119];
     repPoints4 = [-0.3125,-0.0875,0,0.0875,0.3125;
-        99,13,0,13,117];
+        100,15,0,15,120];
     repPoints5 = [-0.3125,-0.0875,0,0.1375,0.3375;
-        99,13,0,15,119];
+        100,15,0,15,120];
     repPoints6 = [-0.3125,-0.0875,0,0.0875,0.3375;
-        101,15,0,13,117];
-    
-    rp1=parWithCorrections(repPoints1);
-    rp1.fix_x_coordinate=false;
-else
-    repPoints1= [  -0.2625   -0.0625         0    0.1375    0.3875;
-        71.8823    8.0028    3.4393   19.2106  138.8119];
-    % 1.1704  -13.4660  970.6693
-    repPoints2 = [ -0.3125   -0.1375         0    0.1375    0.3375;
-        100.1704   21.3737    1.1704   17.6705  107.1909];
-    % 1.5705  -34.1497  980.2241
-    repPoints3 = [ -0.2625   -0.0875         0    0.1125    0.3875;
-        78.0784   12.0635    1.5705   10.1347  135.5243];
-    %    3.4226   10.3652  930.9917
-    repPoints4 = [  -0.3125   -0.0875         0    0.0875    0.3125;
-        91.1006    9.6435    3.4226   11.4575   97.5789];
-    %    1.0e+03 *[    0.0003   -0.0231    1.0188]
-    repPoints5=[   -0.3125   -0.0875         0    0.1375    0.3375;
-        107.0001   10.1094    0.2880   16.3740  108.5418];
-    %3.9129   18.6553  908.4089
-    repPoints6 = [ -0.3125   -0.0875         0    0.0875    0.3375;
-        86.7949    9.2356    3.9129   12.5002  113.6825];
-    
-    rp1=parWithCorrections(repPoints1);
-    rp1.cut_at_e_points = true;
-    rp1.energies = varargin{1};
-    %rp1.p = avrg_par;
-    rp1.fix_x_coordinate = false;
-    
-end
-
+        100,15,0,15,120];
 % #1
-rp1.cut_direction=[1,1,0];
-rp1.dE = 5;
-rp1.dk = 0.05;
+rp1 = parWithCorrections(repPoints1,bragg,[1,1,0],dE,dK);
+
 % #2
-rp2 = parWithCorrections(rp1);
-rp2 = rp2.set_refpar(repPoints2);
-rp2.cut_direction=[1,-1,0];
+rp2 = parWithCorrections(repPoints2,bragg,[1,-1,0],dE,dK);
 % #3
-rp3 = parWithCorrections(rp1);
-rp3 = rp3.set_refpar(repPoints3);
-rp3.cut_direction=[1,0,1];
+rp3 = parWithCorrections(repPoints3,bragg,[1,0,1],dE,dK);
 
 % #4
-rp4 = parWithCorrections(rp1);
-rp4 = rp4.set_refpar(repPoints4);
-rp4.cut_direction=[1,0,-1];
+rp4 = parWithCorrections(repPoints3,bragg,[1,0,-1],dE,dK);
 
 % #5
-rp5 = parWithCorrections(rp1);
-rp5 = rp5.set_refpar(repPoints5);
-rp5.cut_direction=[0,1,1];
+rp5 = parWithCorrections(repPoints3,bragg,[0,1,1],dE,dK);
 
 % #6
-rp6 = parWithCorrections(rp1);
-rp6 = rp6.set_refpar(repPoints6);
-rp6.cut_direction=[0,1,-1];
+rp6 = parWithCorrections(repPoints3,bragg,[0,1,-1],dE,dK);
 
-
-repP={rp2,rp1,rp3,rp4,rp5,rp6};
+repP={rp1,rp2,rp3,rp4,rp5,rp6};
 
 do_fits(data_source,bragg,'<1,1,0>',repP)
 
