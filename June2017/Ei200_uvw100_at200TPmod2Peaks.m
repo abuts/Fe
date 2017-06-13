d12d6890e4fa1f312bd41a75c2cc6ad72adf2763
Alex Buts <Alex.Buts@stfc.ac.uk>
2017-06-13 14:34:10 +0100

    some verified cuts scripts

--- June2017/Ei200_uvw100_at200TPmod2Peaks.m
+++ June2017/Ei200_uvw100_at200TPmod2Peaks.m
@@ -1,49 +1,33 @@
 function Ei200_uvw100_at200TPmod2Peaks(varargin)
 
 data_source= fullfile(pwd,'sqw','Data','Fe_ei200.sqw');
-avrg_par=[0,0,949.35];
-if nargin == 0
-    repPoints1 = [-0.275,-0.075,0,0.175,0.375;
-        123,15,0,19,153];
-    
-    repPoints2=[-0.3375,-0.1875,0,0.1875,0.3375;
-        123,31,0,27,137];
-    repPoints3=[-0.31,-0.12,0,0.12,0.35;
-        140,20,0,20,140];
-    
-    
-    rp1=parWithCorrections(repPoints1);
-    rp1.fix_x_coordinate=false;
-else
-    repPoints1= [ -0.2750   -0.0750         0    0.1750    0.3750;
-                   91.7833   11.7817    3.9750   32.8374  146.5281];
-    repPoints2= [ -0.3375   -0.1875         0    0.1875    0.3375;
-                  126.1526   38.0691   -0.9930   38.8807  127.6135];
+bragg = [2,0,0];
+dE = 5;
+dK = 0.05;
 
-    rp1=parWithCorrections(repPoints1);
-    rp1.cut_at_e_points = true;
-    rp1.energies = varargin{1};
-    %rp1.p = avrg_par;
-    rp1.fix_x_coordinate = false;
-end
+avrg_par=[0,0,949.35];
+
+repPoints1 = [-0.275,-0.075,0,0.175,0.375;
+    125,15,0,20,155];
+
+repPoints2=[-0.3375,-0.1875,0,0.1875,0.3375;
+    125,30,0,25,140];
+repPoints3=[-0.31,-0.12,0,0.12,0.35;
+    140,20,0,20,140];
+
+
 % Cuts
 % #1
-rp1.cut_direction=[1,0,0];
-rp1.dE = 5;
-rp1.dk = 0.05;
+rp1=parWithCorrections(repPoints1,bragg,[1,0,0],dE,dK);
+
 % #2
-rp2 = parWithCorrections(rp1);
-rp2 = rp2.set_refpar(repPoints2);
-rp2.cut_direction=[0,1,0];
-
-rp3 = parWithCorrections(rp1);
-rp3 = rp3.set_refpar(repPoints3);
-rp3.cut_direction=[0,0,1];
-
+rp2=parWithCorrections(repPoints2,bragg,[0,1,0],dE,dK);
+% #3
+rp3=parWithCorrections(repPoints3,bragg,[0,0,1],dE,dK);
 
 repP={rp1,rp2,rp3};
 
-do_fits(data_source,[2,0,0],'<1,0,0>',repP)
+do_fits(data_source,bragg,'<1,0,0>',repP)
 
 
 
