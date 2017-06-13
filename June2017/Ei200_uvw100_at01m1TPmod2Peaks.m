1b8f04199884bee18a543998099d92af29fb2d6c
Alex Buts <Alex.Buts@stfc.ac.uk>
2017-06-13 16:08:34 +0100

    <100> for [0-11] and [011]
    Former-commit-id: 33e8520d8fb8fcaf5350c4d32d75c7cbf793148f

--- June2017/Ei200_uvw100_at01m1TPmod2Peaks.m
+++ June2017/Ei200_uvw100_at01m1TPmod2Peaks.m
@@ -1,4 +1,4 @@
-function Ei200_uvw100_at0m1m1TPmod2Peaks()
+function Ei200_uvw100_at01m1TPmod2Peaks()
 %function [D,x0,alpha,result] = Ei200_uvw100_at110TPmodel2Peaks()
 % simplified verification script, which allows one to check
 % how do sqw cut works
@@ -8,18 +8,18 @@ function Ei200_uvw100_at0m1m1TPmod2Peaks()
 %
 data_source= fullfile(pwd,'sqw','Data','Fe_ei200.sqw');
 
-bragg = [0,-1,-1];  % selected bragg
+bragg = [0,1,-1];  % selected bragg
 dE = 5;
 dK = 0.05;
 %
 % Cut properties:
 %
-repPoints1 = [-0.32,-0.1,0,0.08,0.34;
-    100,15,0,10,120];
-repPoints2= [-0.31,-0.1,0,0.07,0.33;
-    100,15,0,10,120];
-repPoints3 = [-0.3,-0.1,0,0.09,0.33;
-    100,15,0,10,120];
+repPoints1 = [-0.15,-0.04,0,0.11,0.26;
+    35,10,0,10,60];
+repPoints2= [-0.17,-0.07,0,0.08,0.26;
+    40,10,0,10,60];
+repPoints3 = [-0.23,-0.07,0,0.08,0.21;
+    40,10,0,10,50];
 
 rp1 = parWithCorrections(repPoints1,bragg,[1,0,0],dE,dK);
 % #2
@@ -27,7 +27,7 @@ rp2 = parWithCorrections(repPoints2,bragg,[0,1,0],dE,dK);
 % #3
 rp3 = parWithCorrections(repPoints3,bragg,[0,0,1],dE,dK);
 
-repP={rp1,rp2,rp3};
+repP={rp3,rp2,rp1};
 
 do_fits(data_source,bragg,'<1,0,0>',repP)
 
