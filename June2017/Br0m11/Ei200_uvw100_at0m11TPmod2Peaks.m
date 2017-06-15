d201179362b903ca577e2da4f29ba6c88c8492a9
Alex Buts <Alex.Buts@stfc.ac.uk>
2017-06-15 21:19:55 +0100

    added bragg [0,-1,0] to Ei200 fits. Store group of fits as the resulting files.

--- June2017/Br0m11/Ei200_uvw100_at0m11TPmod2Peaks.m
+++ June2017/Br0m11/Ei200_uvw100_at0m11TPmod2Peaks.m
@@ -11,7 +11,7 @@ repPoints1 = [-0.24,-0.09,0,0.1,0.25;
     70,10,0,10,75];
 repPoints2= [-0.28,-0.09,0,0.09,0.24;
     90,10,0,10,55];
-repPoints3 = [-0.28,-0.09,0,0.9,0.23;
+repPoints3 = [-0.28,-0.09,0,0.09,0.23;
     75,10,0,10,50];
 % Cuts
 % #1
@@ -21,6 +21,6 @@ rp2 = parWithCorrections(repPoints2,bragg,[0,1,0],dE,dK);
 % #3
 rp3 = parWithCorrections(repPoints3,bragg,[0,0,1],dE,dK);
 
-repP={rp1,rp2,rp3};
+repP={rp3,rp1,rp2};
 
 do_fits(data_source,bragg,'<1,0,0>',repP)
--- June2017/Br0m11/Ei200_uvw111_at0m11TPmod2Peaks.m
+++ June2017/Br0m11/Ei200_uvw111_at0m11TPmod2Peaks.m
@@ -4,19 +4,19 @@ root = fileparts(pwd);
 data_source= fullfile(root ,'sqw','Data','Fe_ei200.sqw');
 
 
-bragg = [0,1,-1];
+bragg = [0,-1,1];
 dE =5;
 dK =0.05;
 
 
-pefP1 = [-0.16,-0.07,0,0.09,0.25;
-    30,10,0,15,60];
-pefP2 = [-0.20,-0.07,0,0.12,0.23;
-    35,10,0,15,55];
-pefP3 = [-0.2,-0.07,0,0.1,0.2;
-    50,10,0,10,50];
-pefP4 = [-0.19,-0.07,0,0.09,0.24;
-    40,10,0,10,50];
+pefP1 = [-0.29,-0.1,0,0.1,0.24;
+    85,10,0,10,60];
+pefP2 = [-0.27,-0.07,0,0.1,0.26;
+    80,10,0,10,65];
+pefP3 = [-0.23,-0.08,0,0.1,0.3;
+    60,10,0,10,85];
+pefP4 = [-0.26,-0.08,0,0.08,0.29;
+    65,10,0,10,80];
     
 
 rp1 = parWithCorrections(pefP1,bragg,[1,1,1],dE,dK);
@@ -24,6 +24,6 @@ rp2 = parWithCorrections(pefP2,bragg,[1,1,-1],dE,dK);
 rp3 = parWithCorrections(pefP3,bragg,[1,-1,-1],dE,dK);
 rp4 = parWithCorrections(pefP4,bragg,[1,-1,1],dE,dK);
 
-repP={rp4,rp3,rp2,rp1};
+repP={rp1,rp2,rp3,rp4};
 
 do_fits(data_source,bragg,'<1,1,1>',repP)
