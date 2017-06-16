82b6fe0edddb03d89c4f51edc87ea020af5711b1
Alex Buts <Alex.Buts@stfc.ac.uk>
2017-06-16 20:52:48 +0100

    minor modification to run function, specific for fitting bragg 1-1 0 only

--- June2017/Br1m10/Ei400_uvw111_at1m10TPmod2Peaks.m
+++ June2017/Br1m10/Ei400_uvw111_at1m10TPmod2Peaks.m
@@ -22,6 +22,6 @@ rp2 = parWithCorrections(pefP2,bragg,[1,1,-1],dE,dK);
 rp3 = parWithCorrections(pefP3,bragg,[1,-1,-1],dE,dK);
 rp4 = parWithCorrections(pefP4,bragg,[1,-1,1],dE,dK);
 
-repP = {rp4,rp3,rp2,rp1};
+repP = {rp1,rp2,rp3,rp4};
 
 do_fits(data_source,bragg,'<1,1,1>',repP)
