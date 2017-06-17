5c21f557abe8335bb589e683ad56c428314a21e5
Alex Buts <Alex.Buts@stfc.ac.uk>
2017-06-17 21:55:10 +0100

    Added scripts for Bragg101 and run_all script

---
+++ June2017/Br101/Ei200_uvw100_at101TpMod2P.m
@@ -0,0 +1,26 @@
+function Ei200_uvw100_at101Tpmod2P(varargin)
+
+root = fileparts(pwd);
+data_source= fullfile(root ,'sqw','Data','Fe_ei200.sqw');
+
+bragg = [1,0,1];
+dE = 5;
+dK = 0.05;
+
+repPoints1 = [-0.24,-0.09,0,0.1,0.25;
+    70,10,0,10,75];
+repPoints2= [-0.21,-0.09,0,0.09,0.24;
+    40,10,0,10,55];
+repPoints3 = [-0.19,-0.08,0,0.08,0.24;
+    50,10,0,10,40];
+% Cuts
+% #1
+rp1 = parWithCorrections(repPoints1,bragg,[1,0,0],dE,dK);
+% #2
+rp2 = parWithCorrections(repPoints2,bragg,[0,1,0],dE,dK);
+% #3
+rp3 = parWithCorrections(repPoints3,bragg,[0,0,1],dE,dK);
+
+repP={rp2,rp3,rp1};
+
+do_fits(data_source,bragg,'<1,0,0>',repP)
