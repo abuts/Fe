27aa04e9a4a5b9bead160f83fc6ae53d782ada8b
Alex Buts <Alex.Buts@stfc.ac.uk>
2017-06-18 12:38:14 +0100

    Bragg [1,0,1] fully prepared

--- June2017/Br101/Ei200_uvw111_at101TpMod2P.m
+++ June2017/Br101/Ei200_uvw111_at101TpMod2P.m
@@ -1,4 +1,4 @@
-function Ei200_uvw111_at101Tpmod2P(varargin)
+function Ei200_uvw111_at101TpMod2P(varargin)
 
 root = fileparts(pwd);
 data_source= fullfile(root ,'sqw','Data','Fe_ei200.sqw');
@@ -9,13 +9,13 @@ dE =5;
 dK =0.05;
 
 
-pefP1 = [-0.29,-0.1,0,0.1,0.24;
-    85,10,0,10,60];
-pefP2 = [-0.27,-0.07,0,0.1,0.26;
-    80,10,0,10,65];
-pefP3 = [-0.23,-0.08,0,0.1,0.3;
+pefP1 = [-0.24,-0.09,0,0.09,0.29;
+    65,10,0,10,80];
+pefP2 = [-0.24,-0.1,0,0.11,0.29;
     60,10,0,10,85];
-pefP4 = [-0.26,-0.08,0,0.08,0.29;
+pefP3 = [-0.24,-0.09,0,0.1,0.29;
+    60,10,0,10,85];
+pefP4 = [-0.26,-0.11,0,0.1,0.29;
     65,10,0,10,80];
     
 
@@ -24,6 +24,6 @@ rp2 = parWithCorrections(pefP2,bragg,[1,1,-1],dE,dK);
 rp3 = parWithCorrections(pefP3,bragg,[1,-1,-1],dE,dK);
 rp4 = parWithCorrections(pefP4,bragg,[1,-1,1],dE,dK);
 
-repP={rp1,rp2,rp3,rp4};
+repP={rp4,rp3,rp2,rp1};
 
 do_fits(data_source,bragg,'<1,1,1>',repP)
--- June2017/Br101/Ei400_uvw110_at101TpMod2P.m
+++ June2017/Br101/Ei400_uvw110_at101TpMod2P.m
@@ -1,4 +1,4 @@
-function Ei400_uvw110_at101Tpmod2P(varargin)
+function Ei400_uvw110_at101TpMod2P(varargin)
 
 root = fileparts(pwd);
 data_source= fullfile(root ,'sqw','Data','Fe_ei401.sqw');
@@ -9,18 +9,18 @@ dK = 0.05;
 
 
 
-repPoints1= [-0.34,-0.08,0,0.14,0.31;
-    140,15,0,25,90];
-repPoints2 = [-0.26,-0.04,0,0.14,0.36;
-    90,20,0,25,140];
-repPoints3 = [-0.34,-0.11,0,0.14,0.29;
-    115,15,0,20,110];
-repPoints4 = [-0.31,-0.11,0,0.16,0.33;
-    105,15,0,25,120];
-repPoints5 = [-0.36,-0.13,0,0.14,0.26;
-    145,20,0,20,85];
-repPoints6 = [-0.31,-0.11,0,0.11,0.31;
-    130,15,0,15,90];
+repPoints1= [-0.26,-0.11,0,0.11,0.36;
+    90,25,0,15,140];
+repPoints2 = [-0.26,-0.11,0,0.16,0.34;
+    90,20,0,30,140];
+repPoints3 = [-0.31,-0.11,0,0.14,0.31;
+    90,20,0,20,130];
+repPoints4 = [-0.31,-0.11,0,0.11,0.39;
+    105,20,0,15,140];
+repPoints5 = [-0.31,-0.16,0,0.06,0.31;
+    120,25,0,20,110];
+repPoints6 = [-0.29,-0.11,0,0.11,0.34;
+    110,25,0,15,120];
 
 
 rp1 = parWithCorrections(repPoints1,bragg,[1,1,0],dE,dK);
@@ -31,6 +31,6 @@ rp5 = parWithCorrections(repPoints5,bragg,[0,1,1],dE,dK);
 rp6 = parWithCorrections(repPoints6,bragg,[0,1,-1],dE,dK);
 
 
-repP={rp1,rp2,rp3,rp4,rp5,rp6};
+repP={rp6,rp5,rp4,rp3,rp2,rp1};
 
 do_fits(data_source,bragg,'<1,1,0>',repP)
