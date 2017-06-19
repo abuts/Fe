edbf2ed555a16c22349605f4b6245b887f7f8880
Alex Buts <Alex.Buts@stfc.ac.uk>
2017-06-19 19:05:41 +0100

    odd and old bugs in some guess bragg posisions
    Former-commit-id: cf797dc034066efc4ac20f6b1dd5a6d6ba9559dc

---
+++ June2017/Br1m10/run_all.m
@@ -0,0 +1,7 @@
+pwd
+%Ei200_uvw100_at1m10TPmod2Peaks();
+Ei200_uvw110_at1m10TPmod2Peaks();
+Ei200_uvw111_at1m10TPmod2Peaks();
+Ei400_uvw100_at1m10TPmod2Peaks();
+Ei400_uvw110_at1m10TPmod2Peaks();
+Ei400_uvw111_at1m10TPmod2Peaks();
--- June2017/Br200/run_all.m
+++ June2017/Br200/run_all.m
@@ -1,6 +1,7 @@
-%Ei200_uvw100_at200TPmod2Peaks();
-%Ei200_uvw110_at200TPmod2Peaks();
-%Ei200_uvw111_at200TPmod2Peaks();
-Ei400_uvw100_at200TPmod2Peaks();
-Ei400_uvw110_at200TPmod2Peaks();
-Ei400_uvw111_at200TPmod2Peaks();
+pwd
+%Ei200_uvw100_at200TPmod2Peaks
+Ei200_uvw110_at200TPmod2Peaks
+Ei200_uvw111_at200TPmod2Peaks
+Ei400_uvw100_at200TPmod2Peaks
+Ei400_uvw110_at200TPmod2Peaks
+Ei400_uvw111_at200TPmod2Peaks
