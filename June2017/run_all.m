14d1c0b06a42e62b712e0e6b5d91d65dc02b8fdb
Alex Buts <Alex.Buts@stfc.ac.uk>
2017-06-19 15:48:52 +0100

    run_all which indeed caclculates all scripts availible

--- June2017/run_all.m
+++ June2017/run_all.m
@@ -1,16 +1,43 @@
 rootpath = fileparts(mfilename('fullpath'));
 
+p_br110 = fullfile(rootpath,'Br110');
+cd(p_br110);
+pwd
+Ei200_uvw100_at110TPmod2Peaks();
+Ei200_uvw110_at110TPmod2Peaks();
+Ei200_uvw111_at110TPmod2Peaks();
+Ei400_uvw100_at110TPmod2Peaks();
+Ei400_uvw110_at110TPmod2Peaks();
+Ei400_uvw111_at110TPmod2Peaks();
+cd(rootpath);
+p_br1m10 = fullfile(rootpath,'Br1m10');
+cd(p_br1m10);
+pwd
+Ei200_uvw100_at1m10TPmod2Peaks();
+Ei200_uvw110_at1m10TPmod2Peaks();
+Ei200_uvw111_at1m10TPmod2Peaks();
+Ei400_uvw100_at1m10TPmod2Peaks();
+Ei400_uvw110_at1m10TPmod2Peaks();
+Ei400_uvw111_at1m10TPmod2Peaks();
+cd(rootpath);
 
-%Ei200_uvw100_at200TPmod2Peaks();
-%Ei200_uvw110_at200TPmod2Peaks();
-%Ei200_uvw111_at200TPmod2Peaks();
-
-%Ei400_uvw100_at200TPmod2Peaks();
-%Ei400_uvw110_at200TPmod2Peaks();
-%Ei400_uvw111_at200TPmod2Peaks();
+p_br200 = fullfile(rootpath,'Br200');
+cd(p_br200);
+pwd
+Ei200_uvw100_at200TPmod2Peaks
+Ei200_uvw110_at200TPmod2Peaks
+Ei200_uvw111_at200TPmod2Peaks
+Ei400_uvw100_at200TPmod2Peaks
+Ei400_uvw110_at200TPmod2Peaks
+Ei400_uvw111_at200TPmod2Peaks
+%--------------------------------------------------------------------------
 p_br01m1 = fullfile(rootpath,'Br01m1');
 cd(p_br01m1)
 pwd
+Ei200_uvw100_at01m1TPmod2Peaks();
+Ei200_uvw110_at01m1TPmod2Peaks();
+Ei200_uvw111_at01m1TPmod2Peaks();
+
 Ei400_uvw100_at01m1TPmod2Peaks();
 Ei400_uvw110_at01m1TPmod2Peaks();
 Ei400_uvw111_at01m1TPmod2Peaks();
@@ -22,13 +49,7 @@ Ei400_uvw100_at0m1m1TPmod2Peaks();
 Ei400_uvw110_at0m1m1TPmod2Peaks();
 Ei400_uvw111_at0m1m1TPmod2Peaks();
 cd(rootpath);
-p_br0m11 = fullfile(rootpath,'Br0m11');
-cd(p_br0m11)
-pwd
-Ei400_uvw100_at0m11TPmod2Peaks();
-Ei400_uvw110_at0m11TPmod2Peaks();
-Ei400_uvw111_at0m11TPmod2Peaks();
-cd(rootpath);
+%--------------------------------------------------------------------------
 p_br101 = fullfile(rootpath,'Br101');
 cd(p_br101)
 pwd
@@ -38,4 +59,16 @@ Ei200_uvw111_at101TpMod2P();
 Ei400_uvw100_at101TpMod2P();
 Ei400_uvw110_at101TpMod2P();
 Ei400_uvw111_at101TpMod2P();
+
+cd(rootpath);
+p_br0m11 = fullfile(rootpath,'Br0m11');
+cd(p_br0m11)
+pwd
+Ei200_uvw100_at0m11TPmod2Peaks();
+Ei200_uvw110_at0m11TPmod2Peaks();
+Ei200_uvw111_at0m11TPmod2Peaks();
+
+Ei400_uvw100_at0m11TPmod2Peaks();
+Ei400_uvw110_at0m11TPmod2Peaks();
+Ei400_uvw111_at0m11TPmod2Peaks();
 cd(rootpath);
