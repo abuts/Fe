6ee5f0a2c1a686ae7036639f2ca55d720204c166
Alex Buts <Alex.Buts@stfc.ac.uk>
2017-06-20 17:55:11 +0100

    minor changes done on Unix

--- June2017/refit_sw_findJAllBraggEi400.m
+++ June2017/refit_sw_findJAllBraggEi400.m
@@ -1,4 +1,4 @@
-function cp = refit_sw_findJAllBraggEi400(varargin)
+function cut_prod=refit_sw_findJAllBraggEi400(varargin)
 %
 %
 if nargin>0
@@ -11,5 +11,6 @@ end
 bragg_list = {[1,1,0],[1,-1,0],[2,0,0],[0,-1,-1],[0,1,-1],[0,-1,1],[1,0,1]};
 %bragg_list = {[0,-1,1]};
 
-cp = process_cuts();
-cp = cp.refit_sw_findJ(bragg_list,'Fe_ei401',e_min,e_max);
+cut_prod = process_cuts();
+cut_prod = cut_prod.refit_sw_findJ(bragg_list,'Fe_ei401',e_min,e_max);
+
