7373c165336efd90070bb5bb96f27e6bf63c66c1
Alex Buts <Alex.Buts@stfc.ac.uk>
2017-06-22 13:11:49 +0100

    small changes from unix
    Former-commit-id: bdf9aef9a3509e591599d794ad14f6fe97a16ffb

--- June2017/refit_sw_findJAllBraggEi200.m
+++ June2017/refit_sw_findJAllBraggEi200.m
@@ -1,19 +1,13 @@
 function cut_prod=refit_sw_findJAllBraggEi200(varargin)
 %
 %
-if nargin>0
-    e_min = varargin{1};
-    e_max = varargin{2};
-else
-    e_min  = -inf;
-    e_max  = inf;
-end
+
 bragg_list = {[1,1,0],[1,-1,0],[2,0,0],[0,-1,-1],[0,1,-1],[0,-1,1],[1,0,1]};
 %bragg_list = {[0,-1,1]};
 
-cut_prod = process_cuts();
-cut_prod.fit_par_range = [0 0 1 1 0 1 1 1 1 1];
-cut_prod = cut_prod.refit_sw_findJ(bragg_list,'Fe_ei200',e_min,e_max);
+cut_prod = cuts_processor(bragg_list,'Fe_ei200');
+cut_prod.fit_par_range = [0 0 1 1 0 1 1 1 0 0];
+cut_prod = cut_prod.refit_sw_findJ();
 
 
 
