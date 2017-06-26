08532b88eb9cf03e12374b8daaad674344e7b624
Alex Buts <Alex.Buts@stfc.ac.uk>
2017-06-26 20:09:05 +0100

    enhanced inputs for plot_Cuts.m plot_EnCuts.m and plot_dirCut.m and minor changes reflecting the current state of analysis.
    Former-commit-id: a11eec5128f8e67b3cbaaa16a0a18f79df1833f0

--- June2017/Br110/refit_sw_findJBr110.m
+++ June2017/Br110/refit_sw_findJBr110.m
@@ -3,7 +3,9 @@ function cp=refit_sw_findJBr110(varargin)
 %
 
 cp= cuts_processor([1,1,0],'Fe_ei401');
-cp.fit_par_range = [0,0,1,1,0,1,1,1,0,0];
+cp.fit_par_range = [0,0,1,1,0,0,1,1,0,0];
+cp.J0 = 16.;
+
 %cp.J1 = 0;
 %cp.J2 = 0;
 cp = cp.refit_sw_findJ();
--- June2017/Utilities/plot_EnCuts.m
+++ June2017/Utilities/plot_EnCuts.m
@@ -1,10 +1,29 @@
 function  stor=plot_EnCuts(cut_fname,varargin)
 % View group of cuts,
+% Inputs:
+% either:
+%  cut_fname -- the name of the .mat file containing energy cuts 
+% or:
+%              EnCutBlock structure, containing the cuts 
+% or:
+%              The structure, with cuts and fits which may be a source of
+%              an EnCutBlock
+% or:         3 values to build standard EnCut file name namely:
+%             Ei,dE,direction
+% where:  Ei :: the incident energy -- the key part of Fe_eiEi file name
+%         dE :: the energy transfer defining the sequence of cuts
+%   direction:: either 
+%               3-vector defining 
+%               sequence of equivalent directions
+%              (e.g. [1,0,0] corresponging to <1,0,0> plains or [1,1,1])
+%             or
+%              a 3-digit integer, defining a sequence of directions
+%              (e.g 100, 110 or 111);
 %
 %Load the group from hdd if this option is specified
 %
 options = {'-keep_fig','-tight'};
-[ok,mess,keep_fig,tight] = parse_char_options(varargin,options);
+[ok,mess,keep_fig,tight,pars] = parse_char_options(varargin,options);
 if~ok
     error('plot_EnCuts:invalid_argument',mess);
 end
@@ -14,6 +33,15 @@ if isstruct(cut_fname)
     stor = EnCutBlock(cut_fname);
 elseif isa(cut_fname,'EnCutBlock')
     stor = cut_fname;
+elseif isnumeric(cut_fname)
+    Ei = cut_fname;
+    dE = pars{1};
+    dir = pars{2};
+    if numel(dir) == 3
+        dir = 100*dir(1)+10*dir(2)+dir(3);
+    end
+    cut_fname = ['EnCuts_Fe_ei',num2str(Ei),'_dE',num2str(dE),'_dir_!',num2str(dir),'!'];
+    stor = EnCutBlock.load(cut_fname);    
 else
     stor = EnCutBlock.load(cut_fname);
 end
