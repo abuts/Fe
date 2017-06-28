ac4a2e10910051c83241696a9fc3cd69b10e5eab
Alex Buts <Alex.Buts@stfc.ac.uk>
2017-06-28 09:27:04 +0100

    better interface for cut routines

--- June2017/Utilities/rez_name.m
+++ June2017/Utilities/rez_name.m
@@ -9,7 +9,15 @@ else
 end
 [~,fname] = fileparts(data_file);
 caption =@(vector)['[' num2str(vector(1)) num2str(vector(2))  num2str(vector(3)) ']'];
-bragg_name = caption(bragg);
-dir_name  = caption(direction);
+if numel(bragg) == 1
+    bragg_name = ['[', num2str(bragg),']'];
+else
+    bragg_name = caption(bragg);
+end
+if numel(direction) == 1
+    dir_name = ['[', num2str(direction),']'];
+else
+    dir_name  = caption(direction);
+end
 
 name = [prefix,fname,'_bragg_',bragg_name,'_dir_',dir_name];
