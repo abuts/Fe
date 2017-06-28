2285e9cd221b047c35d2dee69803c8e18dec35ba
Alex Buts <Alex.Buts@stfc.ac.uk>
2017-06-28 20:00:45 +0100

    Better run_all parallel jobs scalability
    Former-commit-id: 90e52176d17b5cc94fae5d87e2cc730707093b36

--- June2017/run_all.m
+++ June2017/run_all.m
@@ -2,13 +2,15 @@ function run_all
 
 rootpath = fileparts(mfilename('fullpath'));
 
-fun_list = {@proc_bragg_0m1m1,...
+fun_list = {...
+    @proc_bragg_0m1m1,...
     @proc_bragg_0m11,...
     @proc_bragg_01m1,...
-    @proc_bragg_1m10,...    
     @proc_bragg_10m1,... % checkit
     @proc_bragg_101,...
-    @proc_bragg_110,@proc_bragg_1m10,@proc_bragg_200};
+    @proc_bragg_110E200,@proc_bragg_110E400,...
+    @proc_bragg_1m10E200,@proc_bragg_1m10E400,...
+    @proc_bragg_200E200a,@proc_bragg_200E200b,@proc_bragg_200E400a,@proc_bragg_200E400b};
     
 parfor i=1:numel(fun_list)
     f = fun_list{i};
@@ -17,7 +19,7 @@ end
 
 
 
-function proc_bragg_110(rootpath)
+function proc_bragg_110E200(rootpath)
 
 p_br110 = fullfile(rootpath,'Br110');
 cd(p_br110);
@@ -25,12 +27,20 @@ pwd
 Ei200_uvw100_at110TPmod2Peaks();
 Ei200_uvw110_at110TPmod2Peaks();
 Ei200_uvw111_at110TPmod2Peaks();
+cd(rootpath);
+
+function proc_bragg_110E400(rootpath)
+
+p_br110 = fullfile(rootpath,'Br110');
+cd(p_br110);
+pwd
 Ei400_uvw100_at110TPmod2Peaks();
 Ei400_uvw110_at110TPmod2Peaks();
 Ei400_uvw111_at110TPmod2Peaks();
 cd(rootpath);
 
-function proc_bragg_1m10(rootpath)
+
+function proc_bragg_1m10E200(rootpath)
 
 p_br1m10 = fullfile(rootpath,'Br1m10');
 cd(p_br1m10);
@@ -38,23 +48,52 @@ pwd
 Ei200_uvw100_at1m10TPmod2Peaks();
 Ei200_uvw110_at1m10TPmod2Peaks();
 Ei200_uvw111_at1m10TPmod2Peaks();
+cd(rootpath);
+
+function proc_bragg_1m10E400(rootpath)
+
+p_br1m10 = fullfile(rootpath,'Br1m10');
+cd(p_br1m10);
+pwd
 Ei400_uvw100_at1m10TPmod2Peaks();
 Ei400_uvw110_at1m10TPmod2Peaks();
 Ei400_uvw111_at1m10TPmod2Peaks();
 cd(rootpath);
 
-function proc_bragg_200(rootpath)
+
+function proc_bragg_200E200a(rootpath)
 
 p_br200 = fullfile(rootpath,'Br200');
 cd(p_br200);
 pwd
 Ei200_uvw100_at200TPmod2Peaks
-Ei200_uvw110_at200TPmod2Peaks
 Ei200_uvw111_at200TPmod2Peaks
+cd(rootpath);
+
+function proc_bragg_200E200b(rootpath)
+
+p_br200 = fullfile(rootpath,'Br200');
+cd(p_br200);
+pwd
+Ei200_uvw110_at200TPmod2Peaks
+cd(rootpath);
+
+function proc_bragg_200E400a(rootpath)
+
+p_br200 = fullfile(rootpath,'Br200');
+cd(p_br200);
+pwd
 Ei400_uvw100_at200TPmod2Peaks
-Ei400_uvw110_at200TPmod2Peaks
 Ei400_uvw111_at200TPmod2Peaks
 cd(rootpath);
+function proc_bragg_200E400b(rootpath)
+
+p_br200 = fullfile(rootpath,'Br200');
+cd(p_br200);
+pwd
+Ei400_uvw110_at200TPmod2Peaks
+cd(rootpath);
+
 
 function proc_bragg_01m1(rootpath)
 
