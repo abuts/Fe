fba48558d194572d0aeec14afdcaee6b7f975bfb
Alex Buts <Alex.Buts@stfc.ac.uk>
2017-06-30 09:44:45 +0100

    refit_EnCuts with possibility to exclude some cuts

--- June2017/@cuts_processor/plot_Cuts.m
+++ June2017/@cuts_processor/plot_Cuts.m
@@ -1,15 +1,21 @@
 function  cut_struct = plot_Cuts(obj,energy,direction,varargin)
 % View sequence of cuts correspondent to energy transfer and direction provided
 % as input and return the structure, usually stored with cut sequence
-% inputs: 
+% inputs:
 %
 % options = {'-keep_fig'};
 %
 
-
-
-CutID = [num2str(energy),direction_id(direction)];
+CutID = [num2str(energy(1)),direction_id(direction)];
 
 cut_struct = obj.build_stor_structure_(CutID);
-plot_EnCuts(cut_struct,varargin{:});
+[~,plot_map]=plot_EnCuts(cut_struct,varargin{:});
+
+
+for i=2:numel(energy)
+    CutID = [num2str(energy(i)),direction_id(direction)];
+    
+    cut_struct = obj.build_stor_structure_(CutID);
+    [~,]=plot_EnCuts(cut_struct,plot_map,varargin{:});
+end
 
