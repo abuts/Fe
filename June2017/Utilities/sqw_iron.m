a006de6cc55be69679006e906ca71dde5dd3d292
Alex Buts <Alex.Buts@stfc.ac.uk>
2017-10-04 15:48:18 +0100

    read result of enCut fitting and return it as an array of fit parameters
    Former-commit-id: a0e8a9b1cd59be9ed8fd8a67d08c1059359962a0

--- June2017/Utilities/sqw_iron.m
+++ June2017/Utilities/sqw_iron.m
@@ -71,7 +71,7 @@ weight = (((4/3)*290.6)*idisp{1}) .* (dsho_over_eps (en, wdisp{1}, gamma) .* bos
 
 % Correct for magnetic form factor if requested
 if ff_correct
-    ssqr = (((2*pi/2.87))^2/(16*pi^2))*(qh.^2 + qk.^2 + ql.^2);  % assumes iron lattice parameter = 2.87 Ang
+    ssqr = (((2*pi/2.845))^2/(16*pi^2))*(qh.^2 + qk.^2 + ql.^2);  % assumes iron lattice parameter = 2.845 Ang
     ffpar = [0.0706, 35.008, 0.3589, 15.358, 0.5819, 5.561, -0.0114, 0.1398];
     ffsqr = (ffpar(1)*exp(-ffpar(2)*ssqr)+ffpar(3)*exp(-ffpar(4)*ssqr)+ffpar(5)*exp(-ffpar(6)*ssqr)+ffpar(7)).^2;
     weight = weight .* ffsqr;
