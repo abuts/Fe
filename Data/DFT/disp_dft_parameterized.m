13f92241dec5c10b770d047338b7e803174c78d3
Alex Buts <Alex.Buts@stfc.ac.uk>
2017-10-13 21:31:41 +0100

    trying to resolve issue with interpolation over the whole q-range
    Former-commit-id: 9f690359750ebea5cdf6764ab214982b95810d74

--- Data/DFT/disp_dft_parameterized.m
+++ Data/DFT/disp_dft_parameterized.m
@@ -4,6 +4,7 @@ function wdisp = disp_dft_parameterized(qh,qk,ql,en,varargin)
 persistent hi_grid;
 persistent q_axis;
 persistent e_axis;
+persistent transf_vec;
 %persistent x3;
 %persistent x4;
 if isempty(hi_grid)
@@ -19,24 +20,34 @@ if isempty(hi_grid)
         hi_grid = varargin{1};
     end
     q0 = 1;
+    %-0.7029    1.4075
     q_axis = single(0:q0/100:q0);
     e_axis = single(0:e_max/500:e_max);
-    %[x1,x2,x3,x4] = ndgrid(q_axis,q_axis,q_axis,e_axis);
+    transf_vec = {[0,0,0],[-2,0,0],[2,0,0],...
+        [-1,-1,0],[1,-1,0],[3,-1,0],...
+        [0,2,0],[0,4,0],...
+        [1,1,0],[1,3,0],[1,4,0],...
+        [3,1,0],[3,3,0]};
 end
 % move all vectors into 0-1 quadrant where the interpoland is defined.
 q3 = [qh,qk,ql];
 sq2 = 1/sqrt(2.);
 sm = [sq2,sq2,0;sq2,0,sq2;0,sq2,sq2];
-brav = q3*sm;
-
-
-q3r  = brav-floor(brav);
-
+brav = floor(q3*sm)/sm;
+%q3r  = brav-floor(brav);
 %qr = single(q3r/sm);
-qr = single(q3r);
-qhi = abs(qr(:,1));
-qki = abs(qr(:,2));
-qli = abs(qr(:,3));
+
+qr = single(q3r-brav);
+% qr = zeros(size(q3));
+% for i=1:numel(transf_vec)
+%     qt = q3+transf_vec{i};
+%     valid =qt(:,1)>=0 & qt(:,1)<=1 &qt(:,2)>=0 & qt(:,2) <=1;
+%     qr(valid,:) = qt(valid,:);
+% end
+
+qhi = single(qr(:,1));
+qki = single(qr(:,2));
+qli = single(qr(:,3));
 en  = single(en);
 
 wdisp = interpn(q_axis',q_axis' ,q_axis' ,e_axis',hi_grid,qhi,qki,qli,en);
