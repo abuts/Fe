8ba33e363288bc28a73aa39177233bc3d5594c31
Alex Buts <Alex.Buts@stfc.ac.uk>
2017-10-23 19:54:11 +0100

    Working double Gaussian and DSHO models.

---
+++ Data/DFT/MapDFT_DSHOModel.m
@@ -0,0 +1,106 @@
+function MapDFT_DSHOModel()
+Nq = 100;
+iq = 1:Nq;
+Ne = 50;
+ie = 1:Ne;
+q_min = 0;
+q_max = 2;
+e_min = 10;
+e_max = 680;
+
+dQ    = (q_max-q_min)/(Nq-1);
+dE    = (e_max-e_min)/(Ne-1);
+q  = q_min+(iq-1)*dQ;
+e  = e_min+(ie-1)*dE;
+
+qh = q';
+qk = -ones(size(qh));
+ql = zeros(size(qh));
+en = ones(size(qh));
+
+S_ampl    = zeros(Ne,1)*NaN;
+J0_vsE    = zeros(Ne,1)*NaN;
+Gam_vsE   = zeros(Ne,1)*NaN;
+bg     = zeros(Ne,2)*NaN;
+
+
+J0=0.65;
+Gamma = 0.3;
+S = 1.3;
+kB=8.6173324e-2;
+T = 8;
+
+% J0=2.5;
+% Gamma = 2.5;
+% S = 1;
+
+const = 0;
+grad = 0;
+par= [0,J0,Gamma,S,const,grad];
+for i=1:Ne
+    Ei = en*e(i);
+    fw = disp_dft_parameterized(qh,qk,ql,Ei,[1,0]);
+    w1f = IX_dataset_1d(qh-1,fw,ones(size(qh)));
+    %plot(qh,fw);
+    %Int(i) = sum(fw);
+    try
+        par(1) = e(i);
+        [em,im] = max(fw);
+        qm = qh(im)-1;
+        e2 = e(i)*e(i);
+        e02 = e2/3*(1+2*sqrt(1+3*Gamma*Gamma/e2));
+        e0 = sqrt(e02);
+        par(2) = sqrt(e02)/(1+cos(pi*qm))/8; %J0
+        %par(3) = sqrt(par(2)*par(2)-1)*par(2)/3;  % gamma
+        G2 = Gamma*Gamma;
+        A_Fe = 8*kB*T*290.6/(3.*pi);
+        par(4) = (em/A_Fe)*((e2-e02)^2+4*G2*e2)/(Gamma*e0*e(i));  % S
+        [w1_tf,fp]=fit(w1f,@DSHO_hub,...
+            par,[0,1,1,1,1,1],'fit',[1.e-4,40,1.e-6]);
+        %     [w1_tf,fp]=fit(w1f,@DSHO_hub,...
+        %                 par,[0,1,1,1,1,1],'evaluate');
+        
+    catch
+        continue
+    end
+    acolor('k')
+    plot(w1f)
+    acolor('r')
+    pl(w1_tf);
+    if  ~fp.converged
+        continue
+    end
+    J0_vsE(i)  = fp.p(2);
+    Gam_vsE(i) = abs(fp.p(3));
+    S_ampl(i)  = fp.p(4);
+    
+    %     par(2) = J0_vsE(i);
+    Gamma = Gam_vsE(i);  % gamma
+    %     par(4) = S_ampl(i);
+    bg(i,:) = fp.p(4:5);
+    
+end
+disp_q = IX_dataset_1d(e,J0_vsE);
+disp_q.title = 'DSHO fit. J0 + linear background*0.1';
+%disp_q.x_axis = 'En (mEv)';
+acolor('r')
+plot(disp_q);
+bg_d = IX_dataset_1d(e,0.1*bg(:,1));
+bg_d.title = 'backgound*0.1';
+
+acolor('k');
+pl(bg_d);
+
+
+function y = DSHO_hub(x, p, varargin)
+
+%  use ff,T,Gamma,S,gp,J0
+par = [0,8,p(3),p(4),0,abs(p(2)),0,0,0,0];
+
+%y = sqw_iron (qh,qk,ql,en,par)
+base = ones(size(x));
+en   = base*p(1);
+hl   = base*0;
+hk   = base*(-1);
+y = sqw_iron(x,hk,hl,en,par)+...
+    (p(5)+x*p(6));
--- June2017/Utilities/fit_swTP_model.m
+++ June2017/Utilities/fit_swTP_model.m
@@ -368,9 +368,6 @@ q = sign.*sqrt((en+(0.25*Alpha*Alpha/D2-E0))/D2)- Alpha/(2*D2);
 
 %
 
-function y = TwoGaussAndBkgd(x, p, varargin)
-
-y=(exp(-0.5*((x-p(2))/p(3)).^2)+exp(-0.5*((x+p(2))/p(3)).^2))*(p(1)/sqrt(2*pi)/p(3)) + (p(4)+x*p(5));
 
 
 function bg = lin_bg(x,par)
