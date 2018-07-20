98f2aceb3ce33b6287293194ece8c9a872afcda6
Alex Buts <Alex.Buts@stfc.ac.uk>
2018-07-20 09:31:36 +0100

    Small changes related to presentation on ICM2018

--- 2018April/@EnCutBlock/plot_with_dG.m
+++ 2018April/@EnCutBlock/plot_with_dG.m
@@ -1,4 +1,9 @@
 function fh = plot_with_dG(obj,varargin)
+% Plot enCut with inital fit and complemet it with
+% the plots of the fits, made within the routine.
+%
+% The fit parameters should be changed within the routine
+%
 %
 % if -noextcted option is specified, plot only
 % fittet SW and backgoud
@@ -11,16 +16,29 @@ end
 kk = tobyfit(obj.cuts_list);
 
 fp_arr_sim = obj.fit_param.p;
-fp_arr_sim(6) = fp_arr_sim(6)+5;
-kk = kk.set_bfun(@(x,par)(par(1)+x*par(2)),obj.fit_param.bp);
-kk = kk.set_fun(@sqw_iron,fp_arr_sim,[0,0,1,1,0,1,0,0,0,0]);
-w1D_arr1_sim=kk.simulate('comp');
-fp_arr_sim = obj.fit_param.p;
-fp_arr_sim(6) = fp_arr_sim(6)-5;
-kk = kk.set_bfun(@(x,par)(par(1)+x*par(2)),obj.fit_param.bp);
-kk = kk.set_fun(@sqw_iron,fp_arr_sim,[0,0,1,1,0,1,0,0,0,0]);
-w1D_arr2_sim=kk.simulate('comp');
+%fp_arr_sim(3) = 10;
+fp_arr_sim= [   1.0000    8.0000  100.0000    1.4038*1.25         0   34.6588         0         0         0   0 ];
 
+kk = kk.set_bfun(@(x,par)(par(1)+x*par(2)),obj.fit_param.bp);
+kk = kk.set_fun(@sqw_iron,fp_arr_sim,[0,0,0,1,0,1,0,0,0,0]);
+[w1D_arr1_sim,fitpar1]=kk.simulate('comp');
+%[w1D_arr1_sim,fitpar1]=kk.fit('comp');
+disp('initial fit:');
+disp(obj.fit_param.p)
+disp('shifted + fit (green):');
+disp(fitpar1.p);
+fp_arr_sim = obj.fit_param.p;
+% fp_arr_sim(3) = 100;
+fp_arr_sim=[1.0000    8.0000   10.0000    0.5642*0.74         0   29.7438         0         0         0     0];
+
+kk = kk.set_bfun(@(x,par)(par(1)+x*par(2)),obj.fit_param.bp);
+kk = kk.set_fun(@sqw_iron,fp_arr_sim,[0,0,0,1,0,1,0,0,0,0]);
+[w1D_arr2_sim,fitpar2]=kk.simulate('comp');
+%[w1D_arr2_sim,fitpar2]=kk.fit('comp');
+disp('initial fit:');
+disp(obj.fit_param.p)
+disp('shifted - fit (blue):');
+disp(fitpar2.p);
 
 acolor('k')
 aline('-')
