1603ce99a620d058af5f17e0a8e2c398e107a72b
abuts <Alex.Buts@stfc.ac.uk>
2019-04-18 15:03:46 +0100

    Kun importer modified to produce picture, equivalent to the one, presented for iron

--- Data/DFT/import_Kun.m
+++ Data/DFT/import_Kun.m
@@ -1,14 +1,6 @@
 function [calc_arr,q_cen] = import_Kun(varargin)
-labels   = {'G','H','N','G','P','N'};
-datasets = {'GH.dat','HN.dat','NG.dat','GP.dat','PN.dat'};
-add_lab= {'N','P','G','H','P'};
-add_dat= {'PN.dat','PG.dat','GH.dat','HP.dat'};
-% added PG and HP, all others are just connectors
-
-if nargin>0
-    datasets = {datasets{:},add_dat{:}};
-    labels   = {labels{:},add_lab{2:end}};
-end
+labels   = {'H','P','G','H','N','G','P','N'};
+datasets = {'HP.dat','PG.dat','GH.dat','HN.dat','NG.dat','GP.dat','PN.dat'};
 
 calc_arr = repmat(IX_dataset_2d(),numel(datasets),1);
 q_cen = zeros(3,numel(datasets));
