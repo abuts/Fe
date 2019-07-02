7a57de6103453b7159988b4c600ea74b73b862e8
Alex Buts <Alex.Buts@stfc.ac.uk>
2019-07-02 21:10:20 +0100

    fitting Kun's simulation with DSHO 5J Heisenberg model. In this commit the fit_AllKun script contains all J obtained by fitting DSHO model as input

--- 2018Dec/fit_AllKun.m
+++ 2018Dec/fit_AllKun.m
@@ -31,6 +31,7 @@ for i=1:numel(proj)
     w2tha{i} = sqw_eval(w2all{i},@disp_kun_calc,[1,0,kun_sym(i),kun_sym_dir(i),Kun_width]);
     w2tha{i}.data.e = ones(size(w2tha{i}.data.s));
     plot(w2tha{i});
+    keep_figure;
     ly 0 400
     lz  0 1
 end
@@ -39,15 +40,17 @@ end
 ff = 0; % 1
 T = 8;  % 2
 %gamma = 10; % 3
-gamma = 10;
+gamma = 0.6810;
 gap = 0;    % 5
 %Seff = 2;   % 4
-Seff = 1;
+Seff = 1.4489;
 %J1 = 40;    % 6
-J0 = 40;
-J1 = 5;
-J2 = -5;
-par = [ff, T, gamma, Seff, gap, J0, J1, J2, 0, 0];
+J0 = 51.6079;
+J1 = 1.4083;
+J2 = -8.4407;
+J3 = -0.7448;
+J4 = 0.6047;
+par = [ff, T, gamma, Seff, gap, J0, J1, J2, J3, J4];
 
 kk = tobyfit(w2tha{:});
 kk = kk.set_fun(@sqw_iron,par,[0,0,1,1,0,1,1,1,1,1]);
@@ -57,10 +60,11 @@ kk = kk.set_mc_points(10);
 kk = kk.set_options('listing',1,'fit_control_parameters',[1.e-2;60;1.e-3]);
 %kk = kk.set_options('listing',1,'fit_control_parameters',[1.e-4;20;1.e-4]);
 %profile on;
-[w2D_arr1_tf,fp_arr1]=kk.fit;
-%[w2D_arr1_tf,fp_arr1]=kk.simulate;
+%[w2D_arr1_tf,fp_arr1]=kk.fit;
+[w2D_arr1_tf,fp_arr1]=kk.simulate;
 
 for i=1:numel(w2D_arr1_tf)
     plot(w2D_arr1_tf{i});
+    keep_figure
 end
 
