cb8963f37575be6266b97dbeb6403c35bdb28aa7
Alex Buts <Alex.Buts@stfc.ac.uk>
2019-07-03 09:46:24 +0100

    minor bug in fit_GN_Ei800

--- 2018Dec/fit_GN_Ei800_allCuts.m
+++ 2018Dec/fit_GN_Ei800_allCuts.m
@@ -73,7 +73,6 @@ for i=1:nfp
     else
         cut2fit = cut2fit(valid);
         kun_sym_sel  = kun_sym_dir(valid);
-        bg_par.i_plot(i,valid) = i_plots(valid);        
     end
     [A,err,bg_val,bg_er,fgs,bp,bpsig]=fit_encut(cut2fit,fgs,kun_sym,kun_sym_sel,Kun_width);    
     sv_ampl(i) = A;
