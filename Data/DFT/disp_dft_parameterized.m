af41e1b1b98bd2ea3b5b1ec34bf53a0464dc95d6
Alex Buts <Alex.Buts@stfc.ac.uk>
2020-01-06 09:43:27 +0000

    Kun's simulation for volume data

--- Data/DFT/disp_dft_parameterized.m
+++ Data/DFT/disp_dft_parameterized.m
@@ -5,11 +5,11 @@ function wdisp = disp_dft_parameterized(qh,qk,ql,en,varargin)
 % algorithms.
 % Accepts one or two input parameters:
 % Two (or one) elements Array A containing:
-% A(1) -- the amplitude of theoretical magnetic scattering, used as fitting parameter to 
+% A(1) -- the amplitude of theoretical magnetic scattering, used as fitting parameter to
 %         fit theoretical magnetic scattering and the experimental results
-% A(2) -- if present and == 1 specifies if one needs to apply magnetic form factor corrections, 
+% A(2) -- if present and == 1 specifies if one needs to apply magnetic form factor corrections,
 %         or ignore these corrections if A(2) == 0
-%         if its absent, the magnetic form factor sign should be defined as the second 
+%         if its absent, the magnetic form factor sign should be defined as the second
 %         parameter of the function
 
 persistent hi_grid;
@@ -59,7 +59,7 @@ if min(size(qh)) ~= 1
 else
     do_reshape = false;
     if numel(en) == 1
-        en = repmat(en,numel(qh),1);        
+        en = repmat(en,numel(qh),1);
     end
 end
 qr = [qh,qk,ql];
