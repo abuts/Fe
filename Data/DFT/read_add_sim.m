0a4fc7382ca48a3a5fe58e7b113364b04f25e30c
Alex Buts <Alex.Buts@stfc.ac.uk>
2020-02-21 08:51:39 +0000

    minimal zone simulation data + bugdix in compact3D

--- Data/DFT/read_add_sim.m
+++ Data/DFT/read_add_sim.m
@@ -1,5 +1,6 @@
 function [qx,qy,qz,En,Sig] = read_add_sim()
 filename = 'Fe_add_sim_m.dat';
+%filename = 'sim_range.dat';
 fh = fopen(filename,'rt');
 if fh<0
     error('READ_ADD_SIM:runtime_error',...
@@ -11,4 +12,8 @@ qx=data{1};
 qy = data{2};
 qz = data{3};
 En = floor(data{4});
-Sig = data{5};
+if numel(data) == 5
+    Sig = data{5};
+else
+    Sig = zeros(size(data{4}));
+end
