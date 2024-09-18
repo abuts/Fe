d1a3c74ee3cff173925cb5bcef55040c9e113245
abuts <Alex.Buts@stfc.ac.uk>
2024-09-18 16:55:02 +0100

    reconciled reduction for 200meV, 10_02 EiAndElasticForRuns finished

--- Data/sources/reduction/Mantid_nxspe_2024/MAPS1002_Ei200_reduction.py
+++ Data/sources/reduction/Mantid_nxspe_2024/MAPS1002_Ei200_reduction.py
@@ -34,7 +34,7 @@ class MAPSReduction(ReductionWrapper):
        # a number, energy binning assumed to be absolute (e_min, e_step,e_max)
        #
        prop['incident_energy'] = 200.
-       prop['energy_bins'] =[-20,1,166] # Higher range picks next energy
+       prop['energy_bins'] =[-20,1,162] # Higher range picks next energy
 
        # the range of files to reduce. This range ignored when deployed from autoreduction,
        # unless you going to sum these files. 
