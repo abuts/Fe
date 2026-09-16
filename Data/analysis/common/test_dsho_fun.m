2be28789f01adeb7a2a1d6f0f53bd22b4918deca
abuts <Alex.Buts@stfc.ac.uk>
2026-09-16 19:24:15 +0100

    Almost working normalisation in dE direction. strange problem with accuracy

--- Data/analysis/common/test_dsho_fun.m
+++ Data/analysis/common/test_dsho_fun.m
@@ -1,29 +1,32 @@
 function weight = test_dsho_fun(q,en,par,varargin)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
+enAv  = par(2);
 gamma = par(3);
 A = par(4);
 peak_pos = par(6);
 
 % Dispersion and spectral weight(=Seff/2)
 %[wdisp,idisp] = disp_bcc_hfm(qh,qk,ql,par(4:end));
-enAv  = sum(en(:))/numel(en);
-omg0 = enAv*peak_pos;
-% if gamma*1.1>omg0
-%     gamma = omg0/1.1;
-% end
-%omg0Sq = omg0*omg0;
+%enAv  = sum(en(:))/numel(en);
+omg0 = enAv;
+if gamma*1.1>omg0
+    gamma = omg0/1.1;
+end
+omg0Sq = omg0*omg0;
 gamSq = gamma*gamma;
 %Norm = (4/pi)*A*gamma*sqrt(omg0Sq-gamSq);
-Norm = (8/pi)*A*gamma*omg0/enAv;
+Gr = sqrt(omg0Sq/gamSq -1);
+Norm = A*4*gamSq*Gr/omg0/(pi/2-atan2(1-0.5*omg0Sq/gamSq,Gr));
+%Norm = (8/pi)*A*gamma*omg0/enAv;
 
 %q = hkl_proj(1).transform_hkl_to_img([qh,qk,ql]');
 %qq = omg0Sq*sqrt(q(1,:).^2+q(2,:).^2+q(3,:).^2)';
-qomg = omg0*q(:)/enAv;
-qomgSq = qomg.^2;
+qomg = omg0*q(:)*peak_pos;
+
 % sho shape
-en = en(:)/enAv;
-weight = Norm*qomg.*en./((en.^2-qomgSq).^2+(4*gamSq)*en.^2);
+en = en(:);
+weight = Norm*qomg.*en./(((en-qomg).*(en+qomg)).^2+(4*gamSq)*en.^2);
 %y = ((4/pi)*abs(gam.*en0))./((en.^2-en0.^2).^2 + 4*(gam.*en).^2);
 
 end
