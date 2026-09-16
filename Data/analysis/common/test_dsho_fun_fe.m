7cf959c172504bccd00aadfb5474e1574b05735f
abuts <Alex.Buts@stfc.ac.uk>
2026-09-16 12:01:55 +0100

    working normalisation in Q-direction

--- Data/analysis/common/test_dsho_fun_fe.m
+++ Data/analysis/common/test_dsho_fun_fe.m
@@ -1,28 +1,29 @@
-function weight = test_dsho_fun(q,en,par,varargin)
+function weight = test_dsho_fun_fe(q,en,par,varargin)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
+ec  = par(2);
 gamma = par(3);
 A = par(4);
 peak_pos = par(6);
 
 % Dispersion and spectral weight(=Seff/2)
 %[wdisp,idisp] = disp_bcc_hfm(qh,qk,ql,par(4:end));
-enAv  = sum(en(:))/numel(en);
-omg0 = enAv*peak_pos;
+omg0 = ec*peak_pos;
 % if gamma*1.1>omg0
 %     gamma = omg0/1.1;
 % end
+%enAv  = sum(en(:))/numel(en);
 %omg0Sq = omg0*omg0;
 gamSq = gamma*gamma;
 %Norm = (4/pi)*A*gamma*sqrt(omg0Sq-gamSq);
-Norm = (8/pi)*A*gamma*omg0/enAv;
+Norm = (4/pi)*A*ec*peak_pos;
 
 %q = hkl_proj(1).transform_hkl_to_img([qh,qk,ql]');
 %qq = omg0Sq*sqrt(q(1,:).^2+q(2,:).^2+q(3,:).^2)';
-qomg = omg0*q(:)/enAv;
+qomg = omg0*q(:);
 qomgSq = qomg.^2;
 % sho shape
-en = en(:)/enAv;
+en = en(:);
 weight = Norm*qomg.*en./((en.^2-qomgSq).^2+(4*gamSq)*en.^2);
 %y = ((4/pi)*abs(gam.*en0))./((en.^2-en0.^2).^2 + 4*(gam.*en).^2);
 
