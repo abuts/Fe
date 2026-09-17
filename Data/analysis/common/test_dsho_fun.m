4725829cc4075112f355da71dc26d663208b0cf8
abuts <Alex.Buts@stfc.ac.uk>
2026-09-17 12:00:35 +0100

    working version of test function with energy normalization

--- Data/analysis/common/test_dsho_fun.m
+++ Data/analysis/common/test_dsho_fun.m
@@ -10,23 +10,28 @@ peak_pos = par(6);
 %[wdisp,idisp] = disp_bcc_hfm(qh,qk,ql,par(4:end));
 %enAv  = sum(en(:))/numel(en);
 omg0 = enAv;
-if gamma*1.1>omg0
-    gamma = omg0/1.1;
+
+%omg0Sq = omg0*omg0;
+qomg = omg0*q(:)*peak_pos;
+if any(gamma*1.001>qomg)
+    gamma = min(qomg)/1.001;
 end
-omg0Sq = omg0*omg0;
+
 gamSq = gamma*gamma;
+eg = qomg/gamma;
+egSq = eg.*eg;
 %Norm = (4/pi)*A*gamma*sqrt(omg0Sq-gamSq);
-Gr = sqrt(omg0Sq/gamSq -1);
-Norm = A*4*gamSq*Gr/omg0/(pi/2-atan2(1-0.5*omg0Sq/gamSq,Gr));
+Gr = sqrt((eg - 1).*(eg+1));
+Norm = 0.25*(eg./Gr).*(pi/2-atan((1-0.5*egSq)./Gr));
 %Norm = (8/pi)*A*gamma*omg0/enAv;
 
 %q = hkl_proj(1).transform_hkl_to_img([qh,qk,ql]');
 %qq = omg0Sq*sqrt(q(1,:).^2+q(2,:).^2+q(3,:).^2)';
-qomg = omg0*q(:)*peak_pos;
+
 
 % sho shape
 en = en(:);
-weight = Norm*qomg.*en./(((en-qomg).*(en+qomg)).^2+(4*gamSq)*en.^2);
+weight = (A*gamma./Norm).*qomg.*en./(((en-qomg).*(en+qomg)).^2+(4*gamSq)*en.^2);
 %y = ((4/pi)*abs(gam.*en0))./((en.^2-en0.^2).^2 + 4*(gam.*en).^2);
 
 end
